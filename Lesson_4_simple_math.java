578dac0c8d2ddc0c481b0671687d20511d6b917d
Mithun979362 <979362@pdsb.net>
2024-09-19 12:53:12 -0400

    Create Lesson_4_simple_math.java

---
+++ Lesson_4_simple_math.java
@@ -0,0 +1,18 @@
+public Lesson_4_simple_math {
+    public static void main(String[] args) {
+      int amount=(24);
+      int amountt=(7*4);
+      System.out.println("Two dozen is: " + amount);
+      System.out.println("Twenty-eight is: " + amountt);
+      int amounttt = 8;
+      amounttt = amounttt + 1;
+      System.out.println(amounttt);
+      int a = 8;
+      int b = 3;
+      System.out.println(a*b); 
+      System.out.println(a/b); 
+      double aa = 8.0;
+      int bb = 3;
+      System.out.println(aa/bb);
+    }
+}
