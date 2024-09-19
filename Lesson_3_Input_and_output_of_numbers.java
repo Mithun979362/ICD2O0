0214b7f97e0de7c50dbcf4059c485df149d5db88
Mithun979362 <979362@pdsb.net>
2024-09-19 12:42:19 -0400

    Update Lesson_3_Input_and_output_of_numbers.java

--- Lesson_3_Input_and_output_of_numbers.java
+++ Lesson_3_Input_and_output_of_numbers.java
@@ -1,5 +1,8 @@
 class Lesson_3_Input_and_output_of_numbers {
   public static void main(String [] args) {
+  System.out.println("Welcome to Sanjay's Koala Party!");
+  System.out.print("It's possible Sanjay can distribute " + 12);
+  System.out.println(" cookies for each bowl.");
   int bowl = 6 ;
   int cook =144 ;
   String start=("Sanjay Has ");
