555d210a68536d062bd9930c27ecc4f98b2dd2d6
Mithun979362 <979362@pdsb.net>
2024-09-19 12:39:32 -0400

    Lesson_3_Input_and_output_of_numbers.java

---
+++ Lesson_3_Input_and_output_of_numbers.java
@@ -0,0 +1,11 @@
+class Lesson_3_Input_and_output_of_numbers {
+  public static void main(String [] args) {
+  int bowl = 6 ;
+  int cook =144 ;
+  String start=("Sanjay Has ");
+  String End=(" cookies");
+  String Endd=("The Number of Cookies per bowl is:");
+  System.out.println(start +bowl + " bowls for " +cook+ " cookies");
+  System.out.println(Endd+(144/6));
+  }
+}
