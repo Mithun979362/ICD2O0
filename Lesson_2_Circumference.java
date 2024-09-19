6b0488fb09b9ca54bba3a7e04fce90a87751c886
Mithun979362 <979362@pdsb.net>
2024-09-19 12:12:49 -0400

    Lesson_2_Circumference.java

---
+++ Lesson_2_Circumference.java
@@ -0,0 +1,8 @@
+class Lesson_2_Circumference {
+  public static void main(String [] args) {
+    double circ;
+    double rad =  0.5;
+    circ = 2 * Math.PI * rad;
+    System.out.println("The circumference of a circle with the radius of " + rad +" is " +  circ);
+  }
+}
