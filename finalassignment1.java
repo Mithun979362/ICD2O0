23228d8bbe50ebb9203ebf949287e99a51dd24df
Mithun979362 <979362@pdsb.net>
2024-10-10 13:40:40 -0400

    Update finalassignment1.java

--- finalassignment1.java
+++ finalassignment1.java
@@ -6,10 +6,13 @@
 package assigment1;
 import java.util.Scanner;
 import java.lang.Math.*;
-/**
- *
- * @author 979362
- */
+/*
+Mithun Kumar Arunkumar 979362
+Date: 10 October, 2024
+Course: Grade 10 Comp Sci
+Title: Assignment 1
+Description: A program which outputs a series of numbers based on an algorithm.
+*/
 public class finalassignment1 {
     /* Variable dictionary
     Lengthrec-Length of rectangle
