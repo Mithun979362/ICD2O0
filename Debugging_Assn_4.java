2477c8faae3e97fce76295cf882a04eeadd674b3
Mithun979362 <979362@pdsb.net>
2024-11-07 12:53:34 -0500

    Create Debugging_Assn_4.java

---
+++ Debugging_Assn_4.java
@@ -0,0 +1,12 @@
+class Debugging_Assn_4 {
+  public static void main(String[] args) {
+    // Make any needed corrections to the code below until it runs
+    // without errors.
+
+    String lastMonth = "August";
+
+    System.out.println("The last month was: " + lastMonth);
+
+    //////////// End of tasks /////////////////////
+  }
+}
