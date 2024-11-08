2e5a7968f58ee3068047c6e83754f86da8234226
Mithun979362 <979362@pdsb.net>
2024-11-07 20:45:44 -0500

    Update Debuggin_assn_1.java

--- Debuggin_assn_1.java
+++ Debuggin_assn_1.java
@@ -1,8 +1,8 @@
 /* 
  * Debugging L1: Numbers
  */
-class Debuggin_assn_1 {
+class Main {
   public static void main(String [] args) {
-  System.out.println("6 times 4 = 24");
+    System.out.println("6 times 4 = "+ 6*4);
   }
 }
