bb35225178df5586f84a8db35d4b7994a86704c3
Mithun979362 <979362@pdsb.net>
2024-11-14 14:25:01 -0500

    Update Determining a letter grade.java

--- Determining a letter grade.java
+++ Determining a letter grade.java
@@ -6,19 +6,19 @@ class Determining a letter grade {
    System.out.println("Input your mark as a whole number(0-100)");
    int a = kb.nextInt();
    if (a < 49) {
-            System.out.println(a+ " Is a Letter grade of F.");
+            System.out.println(a+ " is a Letter grade of F.");
             System.out.println("Goodbye");
         } else if (a >= 50 && a <= 59) { 
-            System.out.println(a + " Is a Letter grade of D.");
+            System.out.println(a + " is a Letter grade of D.");
             System.out.println("Goodbye");
         } else if (a >= 60 && a<=69) { 
-            System.out.println(a+ " Is a Letter grade of C.");
+            System.out.println(a+ " is a Letter grade of C.");
             System.out.println("Goodbye");
         } else if (a >= 70 && a <= 79) { 
-            System.out.println(a+ " Is a Letter grade of B.");
+            System.out.println(a+ " is a Letter grade of B.");
             System.out.println("Goodbye");
         } else if (a >= 80 && a<=100) { 
-            System.out.println(a + " Is a Letter grade of A.");
+            System.out.println(a + " is a Letter grade of A.");
             System.out.println("Goodbye");
         } else if (a > 100) { 
             System.out.println("Invalid Input");
