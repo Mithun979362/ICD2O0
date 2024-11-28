b7a16e20efef4a61047ad661370f92f9451dd2a8
Mithun979362 <979362@pdsb.net>
2024-11-28 14:06:30 -0500

    Update assignment2.java

--- assignment2.java
+++ assignment2.java
@@ -2,7 +2,7 @@ package javaapplication2;
 
 import java.util.Scanner;
 
-public class JavaApplication2 {
+public class assignment2 {
     
 /*
 Mithun Kumar Arunkumar 979362
@@ -15,7 +15,49 @@ c (int) The numbers that are being subject to the collatz conjecture
 c1 (int) remembers the beginning number
 c2 (int) Remembers the greatest number of the conjecture
 redo(string) Saves the users input to run the code or not
-*/
+*/package javaapplication2;
+
+import java.util.Scanner;
+
+public class JavaApplication2 {
+    
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String redo = "y";
+
+        while (redo.equalsIgnoreCase("y")) {
+            System.out.print("Enter a whole number: ");
+           
+            int initialValue = kb.nextInt();
+            int currentValue = initialValue;
+            int totalNum = 1;
+            int maxValue = initialValue;
+
+            System.out.print("The Collatz sequence for n = " + String.format("%,d", initialValue) + " is " + String.format("%,d", initialValue));
+
+            while (currentValue != 1) {
+                if (currentValue % 2 == 0) {
+                    currentValue = currentValue / 2;
+                } else {
+                    currentValue = currentValue * 3 + 1;
+                }
+                System.out.print("; " + String.format("%,d", currentValue)); // Format current value
+                totalNum++;
+                if (currentValue > maxValue) {
+                    maxValue = currentValue;
+                }
+            }
+
+            System.out.println();
+            System.out.println("The number of terms = " + String.format("%,d", totalNum)); // Format total number of terms
+            System.out.println("The highest value reached = " + String.format("%,d", maxValue)); // Format max value
+            System.out.print("Do another? (y/n): ");
+            redo = kb.next(); // No need for nextLine() here
+        }
+
+        kb.close();
+    }
+}
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in); 
