9de419687fb6c2a9dd1d591025b5c6f1a61fb757
Mithun979362 <979362@pdsb.net>
2024-11-28 14:07:33 -0500

    Update assignment2.java

--- assignment2.java
+++ assignment2.java
@@ -41,7 +41,7 @@ public class JavaApplication2 {
                 } else {
                     currentValue = currentValue * 3 + 1;
                 }
-                System.out.print("; " + String.format("%,d", currentValue)); // Format current value
+                System.out.print("; " + String.format("%,d", currentValue)); 
                 totalNum++;
                 if (currentValue > maxValue) {
                     maxValue = currentValue;
@@ -49,49 +49,10 @@ public class JavaApplication2 {
             }
 
             System.out.println();
-            System.out.println("The number of terms = " + String.format("%,d", totalNum)); // Format total number of terms
-            System.out.println("The highest value reached = " + String.format("%,d", maxValue)); // Format max value
+            System.out.println("The number of terms = " + String.format("%,d", totalNum)); 
+            System.out.println("The highest value reached = " + String.format("%,d", maxValue)); 
             System.out.print("Do another? (y/n): ");
-            redo = kb.next(); // No need for nextLine() here
-        }
-
-        kb.close();
-    }
-}
-
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in); 
-        String redo = "y"; 
-
-        while (redo.equalsIgnoreCase("y")) { 
-            System.out.println("Enter a whole number:");
-            int c = kb.nextInt();
-            int c1 = c; 
-            int totalnum = 1; 
-            int c2 = c;
-
-            
-            System.out.print("The Collatz sequence for n = " + c1 + " is: " + c1);
-
-            while (c != 1) {
-                if (c % 2 == 0) {
-                    c = c / 2;
-                } else {
-                    c = c * 3 + 1; 
-                }
-                System.out.print(";" + c); 
-                totalnum++; 
-                if (c > c2) {
-                    c2 = c; 
-                }
-            }
-
-            System.out.println(); 
-            System.out.println("The number of terms = " + totalnum);
-            System.out.println("The highest value reached = " + c2);
-            System.out.println("Do another? (y/n)");
-            kb.nextLine(); 
-            redo = kb.nextLine(); 
+            redo = kb.next(); 
         }
 
         kb.close();
